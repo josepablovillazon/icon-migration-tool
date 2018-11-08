342119f10c6daf2f462e389e8be97dbd984833c3
Pablo Villazon <p.villazon@gmail.com>
2018-11-08 09:54:16 -0400

    Initial commit for IconMigrationTool solution

---
+++ IconToolMigrationForm/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace IconToolMigrationForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIconPath = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtIVRPath = new System.Windows.Forms.TextBox();
+            this.txtReport = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtIconPath
+            // 
+            this.txtIconPath.Location = new System.Drawing.Point(70, 49);
+            this.txtIconPath.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtIconPath.Name = "txtIconPath";
+            this.txtIconPath.Size = new System.Drawing.Size(270, 20);
+            this.txtIconPath.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(343, 105);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Migrate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(112, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Migration Icon Tool";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 51);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Icon Folder:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(343, 49);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 19);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(343, 72);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(67, 19);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "browse";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 72);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "IVR Folder:";
+            // 
+            // txtIVRPath
+            // 
+            this.txtIVRPath.Location = new System.Drawing.Point(70, 72);
+            this.txtIVRPath.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtIVRPath.Name = "txtIVRPath";
+            this.txtIVRPath.Size = new System.Drawing.Size(270, 20);
+            this.txtIVRPath.TabIndex = 7;
+            // 
+            // txtReport
+            // 
+            this.txtReport.Location = new System.Drawing.Point(12, 150);
+            this.txtReport.Multiline = true;
+            this.txtReport.Name = "txtReport";
+            this.txtReport.Size = new System.Drawing.Size(403, 148);
+            this.txtReport.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 134);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Migration Report:";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(344, 303);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(2);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(72, 34);
+            this.btnClose.TabIndex = 12;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 345);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtReport);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtIVRPath);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtIconPath);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Icons";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIconPath;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtIVRPath;
+        private System.Windows.Forms.TextBox txtReport;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ IconToolMigrationForm/Form1.cs
@@ -0,0 +1,224 @@
+﻿using Svg;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace IconToolMigrationForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string ToBase64String(Image bmp)
+        {
+            MemoryStream stream = new MemoryStream();
+            IFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, bmp);
+            stream.Position = 0;
+
+            byte[] memBytes = new byte[stream.Length];
+            stream.Read(memBytes, 0, (int)stream.Length);
+            stream.Close();
+
+            return Convert.ToBase64String(memBytes);
+        }
+
+        public static Image DeserializeFromBase64Text(string text)
+        {
+            Image img = null;
+            byte[] memBytes = Convert.FromBase64String(text);
+            IFormatter formatter = new BinaryFormatter();
+            MemoryStream stream = new MemoryStream(memBytes);
+            img = (Image)formatter.Deserialize(stream);
+            stream.Close();
+            return img;
+        }
+
+        private static System.Drawing.Image ResizeImage(System.Drawing.Image image, int width, int height)
+        {
+            //a holder for the result
+            Bitmap result = new Bitmap(width, height, PixelFormat.Format16bppRgb565);
+            // set the resolutions the same to avoid cropping due to resolution differences
+            result.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            //use a graphics object to draw the resized image into the bitmap
+            using (Graphics graphics = Graphics.FromImage(result))
+            {
+                //set the resize quality modes to high quality
+                graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+                graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                //draw the image into the target bitmap
+                graphics.DrawImage(image, 0, 0, result.Width, result.Height);
+            }
+
+            //return the resulting bitmap
+            return result;
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(String.IsNullOrEmpty(txtIconPath.Text))
+            {
+                MessageBox.Show("Icon folder path is required.", "Icon Tool", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (String.IsNullOrEmpty(txtIVRPath.Text))
+            {
+                MessageBox.Show("IVR folder path is required.", "Icon Tool", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            //DirectoryInfo d = new DirectoryInfo(txtPath.Text);
+            //FileInfo[] Files = d.GetFiles("*.bmp");
+
+
+            /*var iconFilePaths = Directory.GetFiles(txtIconPath.Text, "*.bmp", SearchOption.AllDirectories);
+            string IVRPath = txtIVRPath.Text;
+            string result;
+
+            foreach (var iconFilePath in iconFilePaths)
+            {
+                using (var bmp = (Bitmap)Image.FromFile(iconFilePath))
+                {
+                    Bitmap _buffer;
+                    if (bmp.Width != 32 || bmp.Height != 32)
+                        _buffer = ResizeImage(bmp, 32, 32) as Bitmap;
+                    else
+                        _buffer = bmp;
+
+                    result = ToBase64String(_buffer);
+                    string IVRFullPath = IVRPath + "\\" + Path.GetFileNameWithoutExtension(iconFilePath) + ".xml";
+
+                    XmlDocument doc = new XmlDocument();
+                    doc.Load(IVRFullPath);
+                    
+                    XmlNode aNodes = doc.SelectSingleNode("/GlobalLibraryItem/Icon");
+                    aNodes.InnerText = result;
+                    System.IO.File.SetAttributes(IVRFullPath, System.IO.FileAttributes.Normal);
+                    doc.Save(IVRFullPath);
+                }
+            }*/
+            var iconFilePaths = Directory.GetFiles(txtIconPath.Text, "*.*", SearchOption.AllDirectories)
+            .Where(s => s.EndsWith(".bmp") || s.EndsWith(".svg"));
+
+            foreach (var iconFilePath in iconFilePaths) {
+                var currentPath = iconFilePath;
+                if (iconFilePath.Contains(".bmp")) {
+                    handleBMPFiles(iconFilePath);
+                } else
+                if (iconFilePath.Contains(".svg")) {
+                    handleSVGFiles(iconFilePath);
+
+                }
+            }
+
+                MessageBox.Show("MIGRATION HAS SUCCESSFULLY COMPLETED", "Icon Tool", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+        private void handleBMPFiles(string pathIcon) {
+            string IVRPath = txtIVRPath.Text;
+            string result;
+            using (var bmp = (Bitmap)Image.FromFile(pathIcon))
+            {
+                Bitmap _buffer;
+                if (bmp.Width != 32 || bmp.Height != 32)
+                    _buffer = ResizeImage(bmp, 32, 32) as Bitmap;
+                else
+                    _buffer = bmp;
+
+                result = ToBase64String(_buffer);
+                string IVRFullPath = IVRPath + "\\" + Path.GetFileNameWithoutExtension(pathIcon) + ".xml";
+
+                XmlDocument doc = new XmlDocument();
+                doc.Load(IVRFullPath);
+
+                XmlNode aNodes = doc.SelectSingleNode("/GlobalLibraryItem/Icon");
+                aNodes.InnerText = result;
+                System.IO.File.SetAttributes(IVRFullPath, System.IO.FileAttributes.Normal);
+                doc.Save(IVRFullPath);
+            }
+        }
+        private void handleSVGFiles(string pathIcon) {
+            string IVRPath = txtIVRPath.Text;
+            string result;
+            var byteArray = Encoding.ASCII.GetBytes(pathIcon);
+
+            using (var stream = new MemoryStream(byteArray))
+            {
+                var svgDocument = SvgDocument.Open(pathIcon);
+                var bitmap = svgDocument.Draw();
+                Bitmap _buffer;
+                if (bitmap.Width != 32 || bitmap.Height != 32)
+                    _buffer = ResizeImage(bitmap, 32, 32) as Bitmap;
+                else
+                    _buffer = bitmap;
+
+                result = ToBase64String(_buffer);
+                string IVRFullPath = IVRPath + "\\" + Path.GetFileNameWithoutExtension(pathIcon) + ".xml";
+
+                XmlDocument doc = new XmlDocument();
+                try
+                {
+                    doc.Load(IVRFullPath);
+                }
+                catch (Exception ex)
+                {
+                    txtReport.Text += "[SVG] Cannot find: " + IVRFullPath + "\r\n";
+                    return;
+                }
+                
+
+                XmlNode aNodes = doc.SelectSingleNode("/GlobalLibraryItem/Icon");
+                aNodes.InnerText = result;
+                System.IO.File.SetAttributes(IVRFullPath, System.IO.FileAttributes.Normal);
+                doc.Save(IVRFullPath);
+
+                txtReport.Text += "[SVG] Icon successfully loaded: " + IVRFullPath + "\r\n";
+
+            }
+        }
+
+        public void ChooseFolder()
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtIconPath.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChooseFolder();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtIVRPath.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
